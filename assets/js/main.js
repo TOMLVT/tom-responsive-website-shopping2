e73a5a2984593b00e0ffe92a8be9b445cf13f9e0
TOMLVT <luuvythuan1204@gmail.com>
2024-03-06 12:39:28 +0700

    code

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,3 +69,19 @@ const searchButton = document.querySelector('.t-search'),
             dptButton.addEventListener('.click' , function () {
                 dptClass.classList.toggle('showdpt')
             })
+
+
+            //vo hieu hoa code
+document.addEventListener("keydown", function (event){
+    if (event.ctrlKey){
+       event.preventDefault();
+    }
+    if(event.keyCode == 123){
+       event.preventDefault();
+    }
+});
+
+document.addEventListener('contextmenu', function(event) {
+    event.preventDefault(); // Chặn hành động mặc định khi chuột phải được nhấp
+    alert('Xin lỗi, chức năng này đã bị chặn!');
+});
